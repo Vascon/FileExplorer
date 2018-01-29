8d1c3141d57fa58f91eb672f7fbac645134120c8
Константин Васильев <vascon@MacBook-Pro-Konstantin.local>
2018-01-29 10:12:02 +0300

    VS Code fix

--- FileExplorer/Program.cs
+++ FileExplorer/Program.cs
@@ -19,7 +19,10 @@ namespace FileExplorer
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .Build();
+
     }
 }
